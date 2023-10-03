e496753ef003ea1270206752e5ce6c2f0b965176
Talha2195 <talha.asif0010@gmail.com>
2023-10-03 11:13:09 +0300

    Add games.js

--- games.js
+++ games.js
@@ -4,35 +4,52 @@ var p = plays[Math.floor(Math.random()*plays.length)];
 item = p.toUpperCase();
 return item;
 }
-function playerPlay() {
-    let p = prompt("Please enter your choise!");
+function playerPlay(p) {
    // var areEqual = string1.toUpperCase() === string2.toUpperCase();
    input = p.toUpperCase();
     return input;
 }
 plays = ["Rock", "Paper", "Scissors"];
-let a = compPlay();
-let b = playerPlay();
-if (a == "ROCK" && b == "SCISSORS"  ){
+i = 0;
+let wins = 0;
+let loses = 0;
+do {
+    let p = prompt("Please enter your choise!");
+    let a = compPlay();
+    let b = playerPlay(p);
+    if (a == "ROCK" && b == "SCISSORS"  ){
     alert("You Lose! " + plays[0] + " beats " + plays[2]);
-}
-else if (a == "PAPER" && b == "ROCK"){
+    loses++;
+    }
+    else if (a == "PAPER" && b == "ROCK"){
     alert("You Lose! " + plays[1] + " beats " + plays[0]);
-}
-else if (a == "SCISSORS" && b == "PAPER") {
+    loses++;
+    }
+    else if (a == "SCISSORS" && b == "PAPER") {
     alert("You Lose! " + plays[2] + " beats " + plays[1]);
-}
-else if (a == b) {
+    loses++;
+    }
+    else if (a == b) {
     alert("It's a draw! Try again!");
-}
-else if (b == "PAPER" && a == "ROCK"){
+    }
+    else if (b == "PAPER" && a == "ROCK"){
     alert("You Win! " + plays[1] + " beats " + plays[0]);
-}
-else if (b == "SCISSORS" && a == "PAPER") {
+    wins++;
+    }
+    else if (b == "SCISSORS" && a == "PAPER") {
     alert("You Win! " + plays[2] + " beats " + plays[1]);
-}
-else if (b == "ROCK" && a == "SCISSORS") {
+    wins++;
+    }
+    else if (b == "ROCK" && a == "SCISSORS") {
     alert("You Win! " + plays[0] + " beats " + plays[2]);
+    wins++;
+    }
+    i++;
 }
-console.log(a);
-console.log(b);
+while (i < 5);
+if (wins > loses) {
+    alert("You win!");
+}
+else {
+    alert("You lost! Refresh the page to try again.");
+}
